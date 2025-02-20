1b161b8bd821bed81f1dad676c2f95927a001579
Josh Levy <josh.levy@kellogg.northwestern.edu>
2025-02-20 03:48:44 +0000

    started code

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+  get("/", {:controller => "places", :action => "index"})
+  get("/places", {:controller => "places", :action => "index"})
+  get("/places/new", {:controller => "places", :action => "new"})
+  get("/places/:id", {:controller => "places", :action => "show"})
 end
