cf19210e0dfe87958dcd059a2d5886bed8eebad0
Josh Levy <josh.levy@kellogg.northwestern.edu>
2025-02-24 19:18:19 +0000

    finished the code

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -3,7 +3,7 @@ def create
   new_entry = Entry.new
   new_entry.title = params[:title]
   new_entry.description = params[:description]
-  new_entry.occurred_on = params[:date]
+  new_entry.occurred_on = params[:occurred_on]
   new_entry.place_id = params[:place_id]
   new_entry.save
   redirect_to "/places/#{new_entry.place_id}"
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get("/places/new", {:controller => "places", :action => "new"})
   get("/places/:id", {:controller => "places", :action => "show"})
   post("/places/create", {:controller => "places", :action => "create"})
-  post("/entries/create/:place_id", {:controller => "entries", :action => "create"})
+  post("/entries/create/", {:controller => "entries", :action => "create"})
 end
